d7a0c9d771066f658bf6ebb84ee83658a9c98765
yuuTilde <106130874+yuuTilde@users.noreply.github.com>
2022-07-10 05:57:29 +0500

    upd

---
+++ README.md
@@ -0,0 +1,2 @@
+# yuuCalc [![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+<img src="./img/Preview.png">
---
+++ yuucalc.py
@@ -0,0 +1,73 @@
+import PySimpleGUIQt as Sg
+
+
+def main():
+    Sg.theme("Dark")
+
+    FSIZE = [
+        [
+            Sg.Radio("MB", "-SIZE-", key="-MB-"),
+            Sg.Radio("GB", "-SIZE-", default=True, key="-GB-"),
+        ],
+        [Sg.InputText(key="-SIZE-")],
+    ]
+
+    ISPEED = [
+        [
+            Sg.Radio("KB/s", "-SPEED-", key="-KBs-"),
+            Sg.Radio("Mbit/s", "-SPEED-", key="-Mbs-"),
+            Sg.Radio("MB/s", "-SPEED-", default=True, key="-MBs-"),
+        ],
+        [Sg.InputText(key="-SPEED-")],
+    ]
+
+    RESULT = [
+        [Sg.Text("(H:M:S):"), Sg.Text(key="-TIME-")],
+    ]
+
+    DT = [
+        [Sg.Frame("File Size", FSIZE)],
+        [Sg.Frame("Internet Speed", ISPEED)],
+        [Sg.Button("Submit")],
+        [Sg.Frame("Download Time", RESULT)],
+    ]
+
+    layout = [[Sg.TabGroup([[Sg.Tab("Dowload Time", DT)]])]]
+
+    window = Sg.Window(
+        "yuuCalc",
+        layout,
+        resizable=False,
+    )
+
+    while True:
+        event, values = window.read()
+        print(event, values)
+        if event == Sg.WIN_CLOSED:
+            break
+        if event == "Submit":
+            if values["-SIZE-"]:
+                if values["-MB-"]:
+                    size = float(values["-SIZE-"]) / 1000
+                elif values["-GB-"]:
+                    size = float(values["-SIZE-"])
+            if values["-SPEED-"]:
+                if values["-KBs-"]:
+                    speed = float(values["-SPEED-"]) / 125
+                elif values["-Mbs-"]:
+                    speed = float(values["-SPEED-"])
+                elif values["-MBs-"]:
+                    speed = float(values["-SPEED-"]) * 8
+            seconds = 8192 / (speed * 1024) * (size * 1024) * 1.05
+            time = (
+                str(int(seconds / 3600))
+                + ":"
+                + str(int(seconds / 60 % 60))
+                + ":"
+                + str(int(seconds % 60))
+            )
+            window["-TIME-"].update(time)
+
+
+if __name__ == "__main__":
+    main()
