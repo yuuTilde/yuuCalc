92351cfee87d0bba1a84238a976ec366fb5a2e54
yuuTilde <106130874+yuuTilde@users.noreply.github.com>
2022-10-28 00:05:52 +0500

    Add files via upload

---
+++ yuucalc.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b;
+    cout << "File Size (GB):";
+    cin >> a;
+    cout << "Download Speed (Mbit):";
+    cin >> b;
+    float c = (8000 / (b * 1000) * (a * 1000)) / 3600;
+    cout << "Result: " << c << "h" << endl;
+}
