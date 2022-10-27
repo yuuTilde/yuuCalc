b34c14edcb65f124c7cf7604e73f7fcb2668ba17
yuuTilde <106130874+yuuTilde@users.noreply.github.com>
2022-10-28 00:12:55 +0500

    Update yuucalc.cpp

--- yuucalc.cpp
+++ yuucalc.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
     float a, b;
-    cout << "File Size (GB):";
+    cout << "File Size (GB): ";
     cin >> a;
-    cout << "Download Speed (Mbit):";
+    cout << "Download Speed (Mbit): ";
     cin >> b;
     float c = (8000 / (b * 1000) * (a * 1000)) / 3600;
     cout << "Result: " << c << "h" << endl;
